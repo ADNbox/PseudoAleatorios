187a8cab630b3d6b779dd2149d33a858d52da96c
Victor Miranda <victor.m.diniz@gmail.com>
2016-04-05 17:20:44 +0800

    Added files via upload

---
+++ GeneradorCaos.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneradorCaos : MonoBehaviour {
+
+    public int anchoCueva = 5;
+    public int alturaCueva = 5;
+    public int semilla = 26;
+    public bool semillaAleatoria = false;
+    [Range(0, 100)]
+    public int porcentajeMuro = 50;
+
+    private int[,] cueva;
+
+    
+    // Use this for initialization
+
+	void Start () {
+        CrearCueva();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            CrearCueva();
+        }
+	}
+    void CrearCueva() {
+        cueva = new int[anchoCueva, alturaCueva];
+        LlenarCuevaAleatoriamente();
+
+    }
+    void LlenarCuevaAleatoriamente() {
+        Random.seed = (semillaAleatoria) ? Random.Range(int.MinValue, int.MaxValue) : semilla;
+
+        for (int i = 0; i < anchoCueva; i++) {//filas
+            for (int j = 0; j < alturaCueva; j++) {//columnas
+                if (i == 0 || i == (anchoCueva - 1) || j == 0 || j == (alturaCueva - 1))
+                {
+                    cueva[i, j] = 1;
+                }
+                else {
+                    int probabilidad = Random.Range(0, 100);
+                    if (probabilidad < porcentajeMuro)
+                    {
+                        cueva[i, j] = 1;
+                    }
+                    else {
+                        cueva[i, j] = 0;
+
+                    }
+
+                }
+
+            }
+
+        }
+    }
+    void OnDrawGizmos()
+    {
+        if (cueva != null)
+        {
+            for (int i = 0; i < anchoCueva; i++)
+            {//filas
+                for (int j = 0; j < alturaCueva; j++)
+                {//columnas
+                    Gizmos.color = (cueva[i, j] == 0) ? Color.white : Color.black;
+                    Gizmos.DrawCube(new Vector3(i, j, 0f), new Vector3(0.9f, 0.9f, 0.9f));
+
+                }
+            }
+
+        }
+    }
+}
