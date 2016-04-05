2ec6d48478873a9a7fd184e8833dfd3a9e6bf3c4
Victor Miranda <victor.m.diniz@gmail.com>
2016-04-05 17:20:19 +0800

    Added files via upload

---
+++ HelloWorld_PCG.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HelloWorld_PCG : MonoBehaviour {
+
+    public int seed = 1;
+    private string palabraFinal;
+    private List<string> letras = new List<string>();
+    void Awake() {
+        letras.Add("H");
+        letras.Add("o");
+        letras.Add("l");
+        letras.Add("a");
+        letras.Add("M");
+        letras.Add("u");
+        letras.Add("n");
+        letras.Add("d");
+        letras.Add("o");
+
+    }
+
+    // Use this for initialization
+    void Start() {
+
+        Random.seed = seed;
+        for (int i = 0; i < letras.Count; i++) {
+            palabraFinal += letras[i];
+        }
+        Debug.Log("Nuestra palabra inicialmente es: " + palabraFinal);
+
+        palabraFinal = "";
+
+        while (letras.Count > 0) {
+            int index = Random.Range(0, letras.Count);
+            palabraFinal += letras[index];
+            letras.RemoveAt(index);
+
+        }
+        Debug.Log("Nuestra palabra finalmente es: " +palabraFinal);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
